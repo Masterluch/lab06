314987d27a31dfaabb1c74ba0b66a2561802846e
Masterluch <atim2203@gmail.com>
2021-05-09 23:48:32 +0300

    added tests

--- include/print.hpp
+++ include/print.hpp
@@ -3,5 +3,5 @@
 #include <string>
 
 void print(const std::string& text, std::ofstream& out);
-void print(const std::string& text, std::oftream& out = std::cout);
+void print(const std::string& text, std::ostream& out = std::cout);
 
